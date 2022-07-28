88d24bc96d0cd519e8447f484aa3650959ec7054
OoiHongXiang <ooihongxiang@gmail.com>
2022-07-28 13:21:52 +0800

    Extracted the highest overheads.

---
+++ overheads.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import csv
+
+def overheads():
+    file_path = Path.cwd()/"csv_reports"/"overheads-day-45.csv"
+
+    empty_list = []
+
+    with file_path.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        for line in reader:
+            empty_list.append(line)
+
+    empty_list.pop(0)
+
+    percentage_list = []
+
+    for sublist in empty_list:
+        percentage = float(sublist[1])
+        percentage_list.append(percentage)
+
+    highest_percentage = max(percentage_list)    
+    index = percentage_list.index(highest_percentage)
+    highest_category = empty_list[index][0]
+    
+    print(highest_category, highest_percentage)
+
+print(overheads())
