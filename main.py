9e5079f4810420aff65955dc3f3f3c7bf30c3f06
OoiHongXiang <ooihongxiang@gmail.com>
2022-07-29 11:55:44 +0800

    Created .txt writer.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from pathlib import Path
 import api, overheads
 
 def main():
     rate = api.api()
 
-    highest_overheads = overheads.overheads()
+    file_path = Path.cwd()/"summary_report.txt"
+    with file_path.open(mode="w", encoding="UTF-8", newline="") as file:
+        file.writeline(rate)
 
 print(main())
